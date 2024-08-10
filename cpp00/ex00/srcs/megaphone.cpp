f05e4cf790132c946ba8676f1f9c029216f59248
Vinicius Vaccari <viniciusvaccarih@hotmail.com>
2024-08-10 21:27:16 +0100

    phonebook add working

--- cpp00/ex00/srcs/megaphone.cpp
+++ cpp00/ex00/srcs/megaphone.cpp
@@ -2,24 +2,16 @@
 
 int	main(int ac, char **av)
 {
-	int	i = 1;
-
 	if (ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 	{
-		while (av[i])
+		for (int i = 1; av[i]; i++)
 		{
 			for (int j = 0; av[i][j]; j++)
-			{
-				av[i][j] = toupper(av[i][j]);
-				std::cout << av[i][j];
-			}
-			i++;
-			if (av[i])
-				std::cout << " ";
+				std::cout << (char)toupper(av[i][j]);
 		}
 	}
-	std::cout << "\n";
+	std::cout << std::endl;
 	return (0);
 }
